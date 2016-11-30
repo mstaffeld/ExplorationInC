8834b55060343fa5b40e7adad2dcaeeb1a6f3831
Matthew Staffeld <matthew.staffeld@gmail.com>
2016-11-30 08:48:50 -0800

    support for lowercase

--- src/Roman.c
+++ src/Roman.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 #include "Roman.h"
 
 int isRoman(const char* characters)
 {
-	// Valid characters: I,V,X,L,C,D,M
 	char* validCharacters = "IVXLCDM";
 
 	for(int i = 0; i < strlen(characters); i++)
 	{
-		if(strchr(validCharacters, characters[i]) == NULL)
+		if(strchr(validCharacters, toupper(characters[i])) == NULL)
 		{
 			return 0;
 		}
