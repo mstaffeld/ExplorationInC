3f9b05942333d72ea1639c0747241df334d62523
Matthew Staffeld <matthew.staffeld@gmail.com>
2016-11-30 10:12:50 -0800

    returning values, no data structure

--- src/Roman.c
+++ src/Roman.c
@@ -5,8 +5,27 @@
 
 int getValue(const char* romanNumeral)
 {
+	//TODO: make better
 	if (strcmp(romanNumeral, "I") == 0)
 		return 1;
+	
+	if (strcmp(romanNumeral, "V") == 0)                 
+		return 5;
+	
+	if (strcmp(romanNumeral, "X") == 0)                 
+		return 10;
+
+	if (strcmp(romanNumeral, "L") == 0)                 
+		return 50;
+
+	if (strcmp(romanNumeral, "C") == 0)                 
+		return 100;
+
+	if (strcmp(romanNumeral, "D") == 0)                 
+		return 500;
+
+	if (strcmp(romanNumeral, "M") == 0)                 
+		return 1000;
 
 	return -1;
 }
